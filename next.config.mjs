5f9bbeb809e86ea7cca844d8cc08827d66f8539b
lisalescano <lisalescano10@gmail.com>
2025-10-20 15:06:16 -0300

    fix: config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     env: {
       ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
     },
+    experimental: {
+      serverComponentsExternalPackages: [], // Limpia paquetes externos
+  },
     // Otras configuraciones que puedas tener...
   }
   
