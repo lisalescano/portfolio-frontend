073465c815f978f66f598570a2f8dd6fd9897004
lisalescano <lisalescano10@gmail.com>
2025-10-20 15:27:20 -0300

    update: nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
-// next.config.mjs - Agrega esto
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // ← Exportación estática
+  trailingSlash: true,
+  images: {
+      unoptimized: true
+  },
   env: {
       ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
   },
-  // Limitar tamaño de funciones
-  experimental: {
-      serverComponentsExternalPackages: [],
-  },
-  // Forzar standalone si el problema es de tamaño
-  output: 'standalone', // ← PRUEBA ESTO
 }
 
 export default nextConfig
