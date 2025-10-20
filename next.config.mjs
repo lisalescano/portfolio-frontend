fb19634d851a2b1fb036db9cee47f0c51c407670
lisalescano <lisalescano10@gmail.com>
2025-10-20 16:19:58 -0300

    fix:configgg

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Sin output, sin experimental, solo lo básico
-  env: {
-    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
-  },
+ 
 }
 
 export default nextConfig
